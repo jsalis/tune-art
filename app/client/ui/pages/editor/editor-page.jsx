7bc4a7a3ba5bb1bd264c7175057a0b98638bde3f
John Salis <jsalis531@gmail.com>
2024-12-16 00:50:18 -0500

    feat: create basic sequencer

--- app/client/ui/pages/editor/editor-page.jsx
+++ app/client/ui/pages/editor/editor-page.jsx
@@ -1,59 +1,126 @@
-import { useEffect } from "react";
-import { Grid, Flex, Label } from "londo-ui";
+import { useState, useMemo, useEffect } from "react";
+import { Grid, Flex, Box, Label, Button, useCallbackRef } from "londo-ui";
 import * as Tone from "tone";
 
-window.Tone = Tone;
+const notes = ["F4", "Eb4", "C4", "Bb3", "Ab3", "F3"];
+const beatsPerMeasure = 8;
 
 export function EditorPage() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [force, setForce] = useState(0);
+    const [beat, setBeat] = useState(0);
+
+    const currentBeat = beat > 0 ? beat - 1 : beatsPerMeasure - 1;
+
+    const table = useMemo(() => createSequenceTable(notes), []);
+    const synths = useMemo(() => createSynths(notes.length), []);
+
+    const repeatCallback = useCallbackRef((time) => {
+        table.forEach((row, index) => {
+            const synth = synths[index];
+            const note = row[beat];
+
+            if (note.enabled) {
+                synth.triggerAttackRelease(note.note, "8n", time);
+            }
+        });
+
+        setBeat((b) => (b + 1) % beatsPerMeasure);
+    });
+
     useEffect(() => {
-        const synth = new Tone.FMSynth().toDestination();
-        // const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+        Tone.start();
+        Tone.getDestination().volume.rampTo(-10, 0.001);
 
-        // const filter = new Tone.Filter(400, "lowpass").toDestination();
-        // const feedbackDelay = new Tone.FeedbackDelay(0.125, 0.5).toDestination();
+        const transport = Tone.getTransport();
+        transport.bpm.value = 120;
 
-        // synth.connect(filter);
-        // synth.connect(feedbackDelay);
+        // Execute the callback function every eight note.
+        const eventId = transport.scheduleRepeat(repeatCallback, "8n");
 
-        // const keys = new Tone.Players({
-        //     urls: {
-        //         0: "A1.mp3",
-        //         1: "Cs2.mp3",
-        //         2: "E2.mp3",
-        //         3: "Fs2.mp3",
-        //     },
-        //     fadeOut: "64n",
-        //     baseUrl: "https://tonejs.github.io/audio/casio/",
-        // }).toDestination();
-
-        const now = Tone.now();
-
-        Tone.getTransport().bpm.value = 240;
-
-        synth.triggerAttackRelease("C#4", "8n", "+0:0:0");
-        synth.triggerAttackRelease("C#4", "8n", "+0:1:0");
-        synth.triggerAttackRelease("C#4", "8n", "+0:2:0");
-        synth.triggerAttackRelease("B3", "8n.", "+0:3:0");
-        synth.triggerAttackRelease("C#4", "8n", "+0:4:1");
-        synth.triggerAttackRelease("E4", "8n.", "+0:5:1");
-
-        synth.triggerAttackRelease("C#4", "8n", "+0:8:0");
-        synth.triggerAttackRelease("C#4", "8n", "+0:9:0");
-        synth.triggerAttackRelease("C#4", "8n", "+0:10:0");
-        synth.triggerAttackRelease("B3", "8n.", "+0:11:0");
-        synth.triggerAttackRelease("F#4", "8n", "+0:12:1");
-        synth.triggerAttackRelease("E4", "8n.", "+0:13:1");
-
-        return () => synth.disconnect();
+        return () => transport.cancel(eventId);
     }, []);
 
+    const toggleNote = (index) => {
+        const i = Math.floor(index / beatsPerMeasure);
+        const j = index % beatsPerMeasure;
+        const note = table[i][j];
+
+        note.enabled = !note.enabled;
+        setForce((f) => f + 1);
+    };
+
+    const togglePlay = () => {
+        const transport = Tone.getTransport();
+
+        if (isPlaying) {
+            transport.stop();
+            setIsPlaying(false);
+        } else {
+            transport.start();
+            setIsPlaying(true);
+        }
+    };
+
+    const getCellBackground = (index) => {
+        if (index % beatsPerMeasure === currentBeat) {
+            return "gray.1";
+        }
+        return "";
+    };
+
     return (
-        <Grid columns="auto" rows="40px auto 160px" minWidth="1024px" height="100vh">
-            <Flex bg="gray.1" borderBottom="base" justify="space-between" align="center">
-                <Label px={2}>Tune Art</Label>
+        <Grid columns="auto" rows="40px auto 160px" minWidth="512px" height="100vh">
+            <Flex bg="gray.1" borderBottom="base" justify="space-between" align="center" />
+            <Flex overflow="hidden" align="center" justify="center">
+                <Grid p={2} columns={beatsPerMeasure}>
+                    {table.flat().map((note, i) => (
+                        <Flex key={i} p={2} bg={getCellBackground(i)}>
+                            <Box
+                                size={30}
+                                borderRadius="base"
+                                bg={note.enabled ? "primary.base" : "gray.2"}
+                                onClick={() => toggleNote(i)}
+                                cursor="pointer"
+                            />
+                        </Flex>
+                    ))}
+                </Grid>
             </Flex>
-            <Flex overflow="hidden"></Flex>
-            <Flex p={2} gap={2} bg="gray.1" borderTop="base"></Flex>
+            <Flex p={2} gap={2} bg="gray.1" borderTop="base" justify="center">
+                <Flex gap={3} direction="column" align="center">
+                    <Button onClick={togglePlay}>{isPlaying ? "Stop" : "Play"}</Button>
+                    <Label fontSize={3}>{currentBeat + 1}</Label>
+                </Flex>
+            </Flex>
         </Grid>
     );
 }
+
+const createSynths = (count) => {
+    const synths = [];
+
+    for (let i = 0; i < count; i++) {
+        const synth = new Tone.Synth({ oscillator: { type: "square8" } }).toDestination();
+        synths.push(synth);
+    }
+
+    return synths;
+};
+
+const createSequenceTable = (notes) => {
+    const rows = [];
+
+    for (const note of notes) {
+        const row = [];
+        for (let i = 0; i < beatsPerMeasure; i++) {
+            row.push({
+                note: note,
+                enabled: Math.random() < 0.15, // false
+            });
+        }
+        rows.push(row);
+    }
+
+    return rows;
+};
